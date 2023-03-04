fe5fcc6fc968ff804d41726e303aa41cd0362b40
prabhat1001 <prabhatsaraswat24@gmail.com>
2023-03-04 20:23:47 +0530

    few bugs fix and formatting

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
-import './LandingPage.css'
+import React from "react";
+import styled from "styled-components";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <MainPage>
-      <img src='/images/building-1.jpg'></img>
+      <img src="/images/building-1.jpg"></img>
       <TextBox>
-      <Heading>We crush minimal design.</Heading>
-      <SubHeading>MONOCHROME is a creative agency based in Chicago. We developed the Genesis Framework and build mobile-optimized themes for WordPress.</SubHeading>
+        <Heading>We crush minimal design.</Heading>
+        <SubHeading>
+          MONOCHROME is a creative agency based in Chicago. We developed the
+          Genesis Framework and build mobile-optimized themes for WordPress.
+        </SubHeading>
       </TextBox>
-    </MainPage> 
-  )
-}
+    </MainPage>
+  );
+};
 
 const MainPage = styled.div`
- background-color: #000;
- width: 100%;
- height: 100vh;
+  background-color: #000;
+  width: 100%;
+  height: 100vh;
 
-  img{
+  img {
     width: 100%;
     height: 100%;
     filter: opacity(20%) grayscale(100%);
   }
 
   //for medium screens
-  @media (min-width: 426px) and (max-width: 768px){
+  @media (min-width: 426px) and (max-width: 768px) {
     width: fit-content;
   }
 
   //for smaller screens
-  @media (min-width: 320px) and (max-width: 425px){
-
+  @media (min-width: 320px) and (max-width: 425px) {
   }
-
-
 `;
 
 const TextBox = styled.div`
@@ -48,11 +48,11 @@ const TextBox = styled.div`
   /* background-color: #fff; */
 
   //for medium screens
-  @media (min-width: 426px) and (max-width: 768px){
+  @media (min-width: 426px) and (max-width: 768px) {
   }
 
   //for smaller screens
-  @media (min-width: 320px) and (max-width: 425px){
+  @media (min-width: 320px) and (max-width: 425px) {
     left: 5%;
     top: 20%;
     width: 80%;
@@ -70,14 +70,14 @@ const Heading = styled.div`
   color: #fff;
   line-height: 4rem;
   font-weight: 600;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 
   //for medium screens
-  @media (min-width: 426px) and (max-width: 768px){
+  @media (min-width: 426px) and (max-width: 768px) {
   }
 
   //for smaller screens
-  @media (min-width: 320px) and (max-width: 425px){
+  @media (min-width: 320px) and (max-width: 425px) {
     width: 80%;
     height: 60%;
     line-height: 3.5rem;
@@ -88,39 +88,23 @@ const SubHeading = styled.div`
   width: 90%;
   height: 20%;
   /* background-color: brown; */
-  font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
   display: flex;
   align-items: center;
   line-height: 1.2rem;
   font-weight: 500;
   color: #fff;
 
+  //for medium screens
+  @media (min-width: 426px) and (max-width: 768px) {
+  }
 
-//for medium screens
-@media (min-width: 426px) and (max-width: 768px){
-}
-
-//for smaller screens
-@media (min-width: 320px) and (max-width: 425px){
-  width: 100%;
-  font-size: 0.85rem;
-}
-
+  //for smaller screens
+  @media (min-width: 320px) and (max-width: 425px) {
+    width: 100%;
+    font-size: 0.85rem;
+  }
 `;
 export default LandingPage;
 
 
-
-
-
-// //for larger screens
-// @media (min-width: 769px){
-// }
-
-// //for medium screens
-// @media (min-width: 426px) and (max-width: 768px){
-// }
-
-// //for smaller screens
-// @media (min-width: 320px) and (max-width: 425px){
-// }
